76845f76265e1b8b20f159374a8b7f5c1d7be5d3
kulbir-ahluwalia <kulbir@terpmail.umd.edu>
2020-03-06 16:46:45 -0500

    added check obstacle function

--- Dijkstra_rigid.py
+++ Dijkstra_rigid.py
@@ -3,46 +3,41 @@
 # Team - Haixiang Fang          , UID - 116293242
 #        Kulbir Singh Ahluwalia , UID - 116836050
 
-#function returns false when the point is outside the circle
-def circular_obstacle(clearance, radius_rigid_robot, test_point_coord):
-	circle_center = (225,150)
-	test_point_coord_x = test_point_coord[0]
-	test_point_coord_y = test_point_coord[1]
-	augment_distance = radius_rigid_robot + clearance
-
-	distance_from_center = ((test_point_coord_x - circle_center[0])**2 + (test_point_coord_y - circle_center[1])**2)**0.5
-
-	if distance_from_center > (25+augment_distance):
-		return False
-	else:
-		return True
-
-#function returns false when the point is outside the ellipse
-def ellipsoid_obstacle(clearance, radius_rigid_robot, test_point_coord):
-	ellipsoid_center = (150,100)
-	test_point_coord_x = test_point_coord[0]
-	test_point_coord_y = test_point_coord[1]
-	augment_distance = radius_rigid_robot + clearance
-	semi_major_axis = 40
-	semi_minor_axis = 20
-
-	distance_from_center = ((test_point_coord_x - ellipsoid_center[0])**2) / (semi_major_axis**2)
-
-							+ (test_point_coord_y - ellipsoid_center[1])**2 / (semi_minor_axis**2)
-
-
-	if distance_from_center > 1:
-		return False
-	else:
-		return True
-
-
 def find_line_slope_and_intercept(test_point_coord, line_point_1, line_point_2):
     slope = (line_point_2[1]-line_point_1[1])/(line_point_2[0]-line_point_1[0])
     intercept = line_point_1[1]-(slope*line_point_1[0])
     #print(slope,intercept)
     return slope, intercept
 
+#function returns false when the point is outside the circle
+def circular_obstacle(clearance, radius_rigid_robot, test_point_coord):
+    circle_center = (225,150)
+    test_point_coord_x = test_point_coord[0]
+    test_point_coord_y = test_point_coord[1]
+    augment_distance = radius_rigid_robot + clearance
+    
+    distance_from_center = ((test_point_coord_x - circle_center[0])**2 + (test_point_coord_y - circle_center[1])**2)**0.5
+    
+    if distance_from_center > (25+augment_distance):
+        return False
+    else:
+        return True
+
+#function returns false when the point is outside the ellipse
+def ellipsoid_obstacle(clearance, radius_rigid_robot, test_point_coord):
+    ellipsoid_center = (150,100)
+    test_point_coord_x = test_point_coord[0]
+    test_point_coord_y = test_point_coord[1]
+    augment_distance = radius_rigid_robot + clearance
+    semi_major_axis = 40
+    semi_minor_axis = 20
+
+    distance_from_center = ((test_point_coord_x - ellipsoid_center[0])**2) / (semi_major_axis**2) + (test_point_coord_y - ellipsoid_center[1])**2 / (semi_minor_axis**2)
+
+    if distance_from_center > 1:
+        return False
+    else:
+        return True
 
 def rectangle_obstacle(clearance, radius_rigid_robot, test_point_coord):
 
@@ -104,6 +99,56 @@ def rectangle_obstacle(clearance, radius_rigid_robot, test_point_coord):
     else:
         return False
 
+def rhombus_obstacle(clearance, radius_rigid_robot, test_point_coord):
+
+    augment_distance = radius_rigid_robot + clearance
+
+    rhombus_point_1 = [250,25]
+    rhombus_point_2 = [225,40]
+    rhombus_point_3 = [200,25]
+    rhombus_point_4 = [225,10]
+    
+    #We set the flags by testing for a point inside the rectangle
+    #Because the sign for the half plane is unique for every line, we test it by using a point that is confirmed to be inside the rectangle
+    edge1_m_c = find_line_slope_and_intercept(test_point_coord,rhombus_point_1, rhombus_point_2)
+    line1 = test_point_coord[1] - (edge1_m_c[0]*test_point_coord[0]) - (edge1_m_c[1] + (augment_distance*0.8575))
+    #print(line1)
+    if line1>=0:
+        flag1 = False
+    else:
+        flag1 = True
+        
+    
+    edge2_m_c = find_line_slope_and_intercept(test_point_coord,rhombus_point_2,rhombus_point_3)
+    line2 = test_point_coord[1] - (edge2_m_c[0]*test_point_coord[0]) - (edge2_m_c[1] + (augment_distance*0.8575))  
+    #print(line2)
+    if line2>=0:
+        flag2 = False
+    else:
+        flag2 = True
+    
+    
+    edge3_m_c = find_line_slope_and_intercept(test_point_coord,rhombus_point_3,rhombus_point_4)
+    line3 = test_point_coord[1] - (edge3_m_c[0]*test_point_coord[0]) - (edge3_m_c[1] - (augment_distance*0.8575))
+    #print(line3)
+    if line3>=0:
+        flag3 = True
+    else:
+        flag3 = False
+    
+    
+    edge4_m_c = find_line_slope_and_intercept(test_point_coord,rhombus_point_4,rhombus_point_1)
+    line4 = test_point_coord[1] - (edge4_m_c[0]*test_point_coord[0]) - (edge4_m_c[1] - (augment_distance*0.8575))
+    #print(line4)
+    if line4>=0:
+        flag4 = True
+    else:
+        flag4 = False
+        
+    if flag1 and flag2 and flag3 and flag4:
+        return True
+    else:
+        return False
 
 def nonconvex_obstacle_right_half(clearance, radius_rigid_robot, test_point_coord):
 
@@ -177,7 +222,6 @@ def nonconvex_obstacle_right_half(clearance, radius_rigid_robot, test_point_coor
     else:
         return False
 
-
 def nonconvex_obstacle_left_half(clearance, radius_rigid_robot, test_point_coord):
 
     augment_distance = radius_rigid_robot + clearance
@@ -194,10 +238,10 @@ def nonconvex_obstacle_left_half(clearance, radius_rigid_robot, test_point_coord
     #print(line1)
     if line1>=0:
         flag1 = True
-        print("True")
+        # print("True")
     else:
         flag1 = False
-        print("False")
+        # print("False")
         
     
     edge2_m_c = find_line_slope_and_intercept(test_point_coord,nonconvex_point_2,nonconvex_point_3)
@@ -205,10 +249,10 @@ def nonconvex_obstacle_left_half(clearance, radius_rigid_robot, test_point_coord
     #print(line2)
     if line2>=0:
         flag2 = False
-        print("False")
+        # print("False")
     else:
         flag2 = True
-        print("True")
+        # print("True")
     
     #edge 3 is not augmented with clearance+robot_radius since its inside the nonconvex polygon
     edge3_m_c = find_line_slope_and_intercept(test_point_coord,nonconvex_point_3,nonconvex_point_4)
@@ -216,10 +260,10 @@ def nonconvex_obstacle_left_half(clearance, radius_rigid_robot, test_point_coord
     #print(line3)
     if line3>=0:
         flag3 = False
-        print("False")
+        # print("False")
     else:
         flag3 = True
-        print("True")
+        # print("True")
     
     
     edge4_m_c = find_line_slope_and_intercept(test_point_coord,nonconvex_point_4,nonconvex_point_1)
@@ -227,17 +271,31 @@ def nonconvex_obstacle_left_half(clearance, radius_rigid_robot, test_point_coord
     #print(line4)
     if line4>=0:
         flag4 = True
-        print("True")
+        # print("True")
     else:
         flag4 = False
-        print("False")
+        # print("False")
         
     if flag1 and flag2 and flag3 and flag4:
         return True
     else:
         return False
 
-
+def test_point_obstacle_check(clearance, radius_rigid_robot, test_point_coord):
+    if circular_obstacle(clearance, radius_rigid_robot, test_point_coord):
+        return True
+    elif ellipsoid_obstacle(clearance, radius_rigid_robot, test_point_coord):
+        return True
+    elif rectangle_obstacle(clearance, radius_rigid_robot, test_point_coord):
+        return True
+    elif rhombus_obstacle(clearance, radius_rigid_robot, test_point_coord):
+        return True
+    elif nonconvex_obstacle_right_half(clearance, radius_rigid_robot, test_point_coord):
+        return True
+    elif nonconvex_obstacle_left_half(clearance, radius_rigid_robot, test_point_coord):
+        return True
+    else:
+        return False
 
 
 
