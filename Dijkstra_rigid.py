50f884c94c92263d09cb9ae823cf2c233f9846b2
kulbir-ahluwalia <kulbir@terpmail.umd.edu>
2020-03-06 15:11:13 -0500

    added nonconvex obstacle detection function that works

--- Dijkstra_rigid.py
+++ Dijkstra_rigid.py
@@ -17,7 +17,6 @@ def circular_obstacle(clearance, radius_rigid_robot, test_point_coord):
 	else:
 		return True
 
-
 #function returns false when the point is outside the ellipse
 def ellipsoid_obstacle(clearance, radius_rigid_robot, test_point_coord):
 	ellipsoid_center = (150,100)
@@ -37,60 +36,207 @@ def ellipsoid_obstacle(clearance, radius_rigid_robot, test_point_coord):
 	else:
 		return True
 
-def square_obstacle(clearance, radius_rigid_robot, test_point_coord):
 
-	circle_center = (225,150)
-	augment_distance = radius_rigid_robot + clearance
-
-	square_point_1 = [100,38.66025]
-	square_point_2 = [35.0481,76.1603]
-	square_point_3 = [30.0481,67.5]
-	square_point_4 = [95,30]
-
-	# edge1 = point_wrt_line(test_point_coord,square_point_2,square_point_1)
-	# edge2 = point_wrt_line(test_point_coord,square_point_3,square_point_2)
-	# edge3 = point_wrt_line(test_point_coord,square_point_4,square_point_3)
-	# edge4 = point_wrt_line(test_point_coord,square_point_1,square_point_4)
-
-	edge1 = point_wrt_line(test_point_coord,square_point_1,square_point_2)
-	edge2 = point_wrt_line(test_point_coord,square_point_2,square_point_3)
-	edge3 = point_wrt_line(test_point_coord,square_point_3,square_point_4)
-	edge4 = point_wrt_line(test_point_coord,square_point_4,square_point_1)
+def find_line_slope_and_intercept(test_point_coord, line_point_1, line_point_2):
+    slope = (line_point_2[1]-line_point_1[1])/(line_point_2[0]-line_point_1[0])
+    intercept = line_point_1[1]-(slope*line_point_1[0])
+    #print(slope,intercept)
+    return slope, intercept
 
 
-	if (not(edge1 or edge2)) and (edge3 and edge4):
-		return True
+def rectangle_obstacle(clearance, radius_rigid_robot, test_point_coord):
 
-	else:
-		return False
+    circle_center = (225,150)
+    augment_distance = radius_rigid_robot + clearance
+
+    rectangle_point_1 = [100,38.66025]
+    rectangle_point_2 = [35.0481, 76.1603]
+    rectangle_point_3 = [30.0481, 67.5]
+    rectangle_point_4 = [95,30]
+    
+    #We set the flags by testing for a point inside the rectangle
+    #Because the sign for the half plane is unique for every line, we test it by using a point that is confirmed to be inside the rectangle
+    edge1_m_c = find_line_slope_and_intercept(test_point_coord,rectangle_point_1,rectangle_point_2)
+    line1 = test_point_coord[1] - (edge1_m_c[0]*test_point_coord[0]) - (edge1_m_c[1] + (augment_distance*0.5*(3**2)))
+    #print(line1)
+    if line1>=0:
+        flag1 = False
+        #print("False")
+    else:
+        flag1 = True
+        #print("True")
+        
+    
+    edge2_m_c = find_line_slope_and_intercept(test_point_coord,rectangle_point_2,rectangle_point_3)
+    line2 = test_point_coord[1] - (edge2_m_c[0]*test_point_coord[0]) - (edge2_m_c[1] + (augment_distance*0.5))  
+    #print(line2)
+    if line2>=0:
+        flag2 = False
+        #print("False")
+    else:
+        flag2 = True
+        #print("True")
+    
+    
+    edge3_m_c = find_line_slope_and_intercept(test_point_coord,rectangle_point_3,rectangle_point_4)
+    line3 = test_point_coord[1] - (edge3_m_c[0]*test_point_coord[0]) - (edge3_m_c[1] - (augment_distance*0.5*(3**2)))
+    #print(line3)
+    if line3>=0:
+        flag3 = True
+        #print("True")
+    else:
+        flag3 = False
+        #print("False")
+    
+    
+    edge4_m_c = find_line_slope_and_intercept(test_point_coord,rectangle_point_4,rectangle_point_1)
+    line4 = test_point_coord[1] - (edge4_m_c[0]*test_point_coord[0]) - (edge4_m_c[1] - (augment_distance*0.5))
+    #print(line4)
+    if line4>=0:
+        flag4 = True
+        #print("True")
+    else:
+        flag4 = False
+        #print("False")
+        
+    if flag1 and flag2 and flag3 and flag4:
+        return True
+    else:
+        return False
 
 
+def nonconvex_obstacle_right_half(clearance, radius_rigid_robot, test_point_coord):
 
-# def point_wrt_line(test_point_coord, line_point_1, line_point_2):
-# 	a = line_point_2[1]-line_point_1[1]
-# 	b = line_point_1[0]-line_point_2[0]
-# 	c = (line_point_2[1]*line_point_1[0])-(line_point_2[0]*line_point_1[1])
+    augment_distance = radius_rigid_robot + clearance
 
-# 	line = a*test_point_coord[0] + b*test_point_coord[1] + c
+    nonconvex_point_1 = [100,150]
+    nonconvex_point_2 = [75,185]
+    nonconvex_point_3 = [60,185]
+    nonconvex_point_4 = [50,150]
+    nonconvex_point_5 = [75,120]
+    
+    #We set the flags by testing for a point inside the rectangle
+    #Because the sign for the half plane is unique for every line, we test it by using a point that is confirmed to be inside the nonconvex_obstacle
+    edge1_m_c = find_line_slope_and_intercept(test_point_coord,nonconvex_point_1, nonconvex_point_2)
+    line1 = test_point_coord[1] - (edge1_m_c[0]*test_point_coord[0]) - (edge1_m_c[1] + (augment_distance*0.58124))
+    #print(line1)
+    if line1>=0:
+        flag1 = False
+        # print("False")
+    else:
+        flag1 = True
+        # print("True")
+        
+    
+    edge2_m_c = find_line_slope_and_intercept(test_point_coord,nonconvex_point_2,nonconvex_point_3)
+    line2 = test_point_coord[1] - (edge2_m_c[0]*test_point_coord[0]) - (edge2_m_c[1] + (augment_distance*1))  
+    #print(line2)
+    if line2>=0:
+        flag2 = False
+        # print("False")
+    else:
+        flag2 = True
+        # print("True")
+    
+    #edge 3 is not augmented with clearance+robot_radius since its inside the nonconvex polygon
+    edge3_m_c = find_line_slope_and_intercept(test_point_coord,nonconvex_point_3,nonconvex_point_4)
+    line3 = test_point_coord[1] - (edge3_m_c[0]*test_point_coord[0]) - (edge3_m_c[1] + (augment_distance*0))
+    #print(line3)
+    if line3>=0:
+        flag3 = False
+        # print("False")
+    else:
+        flag3 = True
+        # print("True")
+    
+    
+    edge4_m_c = find_line_slope_and_intercept(test_point_coord,nonconvex_point_4,nonconvex_point_5)
+    line4 = test_point_coord[1] - (edge4_m_c[0]*test_point_coord[0]) - (edge4_m_c[1] - (augment_distance*0.64018))
+    #print(line4)
+    if line4>=0:
+        flag4 = True
+        # print("True")
+    else:
+        flag4 = False
+        # print("False")
 
-# 	if line>=0:
-# 		return True #on the left of the line, or on the line
+    edge5_m_c = find_line_slope_and_intercept(test_point_coord,nonconvex_point_5,nonconvex_point_1)
+    line5 = test_point_coord[1] - (edge5_m_c[0]*test_point_coord[0]) - (edge5_m_c[1] - (augment_distance*0.640184))
+    #print(line4)
+    if line5>=0:
+        flag5 = True
+        # print("True")
+    else:
+        flag5 = False
+        # print("False")
 
-# 	else:
-# 		return False #D<0, on right of the line
+
+        
+    if flag1 and flag2 and flag3 and flag4 and flag5:
+        return True
+    else:
+        return False
 
 
-def point_wrt_line(test_point_coord, line_point_1, line_point_2):
-	slope = (line_point_2[1]-line_point_1[1])/(line_point_2[0]-line_point_1[0])
-	intercept = line_point_1[1]-(slope*line_point_1[0])
-	#print(slope,intercept)
+def nonconvex_obstacle_left_half(clearance, radius_rigid_robot, test_point_coord):
 
-	line = test_point_coord[1] - (slope*test_point_coord[0]) - intercept
-	if line>=0:
-		return True #on the left of the line, or on the line
+    augment_distance = radius_rigid_robot + clearance
 
-	else:
-		return False #D<0, on right of the line
+    nonconvex_point_1 = [50,150]
+    nonconvex_point_2 = [60,185]
+    nonconvex_point_3 = [25,185]
+    nonconvex_point_4 = [20,120]  
+    
+    #We set the flags by testing for a point inside the rectangle
+    #Because the sign for the half plane is unique for every line, we test it by using a point that is confirmed to be inside the nonconvex_obstacle
+    edge1_m_c = find_line_slope_and_intercept(test_point_coord,nonconvex_point_1, nonconvex_point_2)
+    line1 = test_point_coord[1] - (edge1_m_c[0]*test_point_coord[0]) - (edge1_m_c[1] + (augment_distance*0))
+    #print(line1)
+    if line1>=0:
+        flag1 = True
+        print("True")
+    else:
+        flag1 = False
+        print("False")
+        
+    
+    edge2_m_c = find_line_slope_and_intercept(test_point_coord,nonconvex_point_2,nonconvex_point_3)
+    line2 = test_point_coord[1] - (edge2_m_c[0]*test_point_coord[0]) - (edge2_m_c[1] + (augment_distance*1))  
+    #print(line2)
+    if line2>=0:
+        flag2 = False
+        print("False")
+    else:
+        flag2 = True
+        print("True")
+    
+    #edge 3 is not augmented with clearance+robot_radius since its inside the nonconvex polygon
+    edge3_m_c = find_line_slope_and_intercept(test_point_coord,nonconvex_point_3,nonconvex_point_4)
+    line3 = test_point_coord[1] - (edge3_m_c[0]*test_point_coord[0]) - (edge3_m_c[1] + (augment_distance*0.0767))
+    #print(line3)
+    if line3>=0:
+        flag3 = False
+        print("False")
+    else:
+        flag3 = True
+        print("True")
+    
+    
+    edge4_m_c = find_line_slope_and_intercept(test_point_coord,nonconvex_point_4,nonconvex_point_1)
+    line4 = test_point_coord[1] - (edge4_m_c[0]*test_point_coord[0]) - (edge4_m_c[1] - (augment_distance*0.7071))
+    #print(line4)
+    if line4>=0:
+        flag4 = True
+        print("True")
+    else:
+        flag4 = False
+        print("False")
+        
+    if flag1 and flag2 and flag3 and flag4:
+        return True
+    else:
+        return False
+
 
 
 
